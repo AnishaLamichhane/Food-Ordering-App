c9cffb37d413b33419458d96e792c42386729547
AnishaLamichhane <ansalamichhane111@gmail.com>
2022-04-18 20:05:46 +0545

    Networking( Creating URL Request).

---
+++ Food Ordering App/Networking/Method.swift
@@ -0,0 +1,14 @@
+//
+//  Method.swift
+//  Food Ordering App
+//
+//  Created by Anisha Lamichhane on 4/18/22.
+//
+
+import Foundation
+enum Method : String {
+    case get = "GET"
+    case post = "POST"
+    case delete  = "DELETE"
+    case patch = "PATCH"
+}
---
+++ Food Ordering App/Networking/NetworkService.swift
@@ -0,0 +1,42 @@
+//
+//  NetworkService.swift
+//  Food Ordering App
+//
+//  Created by Anisha Lamichhane on 4/17/22.
+//
+
+import Foundation
+
+struct NetworkService {
+    private func createRequest(route: Route, method: Method, parameters: [String: Any]? = nil) -> URLRequest? {
+        let urlString = Route.baseUrl + route.description
+        guard let url = urlString.asURL else {
+            print("this is not a valid url")
+            return nil
+        }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.httpMethod = method.rawValue
+        
+        if let params = parameters  {
+            switch method {
+                
+                
+            case .get:
+                var urlComponent = URLComponents(string: urlString)
+                urlComponent?.queryItems = params.map {
+                    URLQueryItem(name: $0, value: "\($1)")
+                }
+                
+                urlRequest.url = urlComponent?.url
+                
+                
+            case .post, .delete, .patch:
+                let bodyData = try? JSONSerialization.data(withJSONObject: params, options: [])
+                urlRequest.httpBody = bodyData
+            }
+        }
+        
+        return urlRequest
+    }
+}
